970b65535acdbd27571e3ff5435ca4ee9687f23d
Hussien Mosad <hussienmosad19@gmail.com>
2026-04-29 02:19:08 +0300

    Add files via upload

---
+++ C#/Problem-1.cs
@@ -0,0 +1,54 @@
+﻿// Problem 1: Count Special Characters
+
+using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static string ReadString()
+        {
+            string Word;
+            Console.Write("Please Enter Your String: ");
+            Word = Console.ReadLine();
+            return Word;
+        }
+
+        static void CountCharacters(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                Console.WriteLine("Empty input.");
+                return;
+            }
+
+            int uppercase = 0, lowercase = 0, digits = 0, specialCharacters = 0;
+
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c))
+                    digits++;
+                else if (char.IsLower(c))
+                    lowercase++;
+                else if (char.IsUpper(c))
+                    uppercase++;
+                else
+                    specialCharacters++;
+            }
+
+            Console.WriteLine($"Uppercase: {uppercase}");
+            Console.WriteLine($"Lowercase: {lowercase}");
+            Console.WriteLine($"Digits: {digits}");
+            Console.WriteLine($"Special Characters: {specialCharacters}");
+        }
+
+        static void Main(string[] args)
+        {
+            string Word = ReadString();
+            Console.WriteLine($"You entered: {Word}");
+            CountCharacters(Word);
+
+            Console.ReadKey();
+        }
+    }
+}
